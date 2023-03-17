9bd21ea43e7989d1bbfe10ed10deab4c0216a15a
FERNANDO <joferguzvas@gmail.com>
2023-03-17 03:27:05 -0600

    muestra campos

--- resources/views/pages/user-profile.blade.php
+++ resources/views/pages/user-profile.blade.php
@@ -1,10 +1,6 @@
 @extends('./Home')
 
 @section('conteiner-middle')
-<div>
-    <span>Hila</span>
-
-</div>
 <div class="col-md-12 mb-3">
     <form class="user" method="GET" action="">
         @csrf
@@ -12,13 +8,13 @@
 <div class="col-md-12">
     <div class="mb-3">
         <label class="form-label">Name</label>
-        <input type="text" class="form-control add-billing-address-input" name="name" id="name">
+        <input type="text" class="form-control add-billing-address-input" name="name" id="name" value="{{auth()->user()->name}}">
     </div>
 </div>
 <div class="col-md-12">
     <div class="mb-3">
         <label class="form-label">Email</label>
-        <input type="email" class="form-control" name="email">
+        <input type="email" class="form-control" name="email" value='{{auth()->user()->email}}'>
     </div>
 </div>
 <div class="col-12">
